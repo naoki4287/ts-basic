9c8ae4a9309a9fd4f7f340718f205c3eafac5f9b
github naoki4287 <github@waka.naoki86@icloud.com>
2022-03-08 12:35:44 +0900

    ✨ 仮引数の型の付け方についてべんきょうしました。　

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,20 @@
 // world.sayHello(root);
 
 // 03.基本の型定義
-import { unknownSample, anySample, notExistSample, primitiveSample } from "./basic";
+// import { unknownSample, anySample, notExistSample, primitiveSample } from "./basic";
 
-unknownSample()
-anySample()
-notExistSample()
-primitiveSample()
+// unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+
+// 04.関数の型定義
+import { logMessage } from "./function/basic"
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from "./function/parameter"
+
+logMessage("Hello TypeScript!")
+isUserSignedIn("ABC", "Torahack")
+isUserSignedIn("DEF")
+isUserSignedIn2("ABC")
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Function parameters sample 5:', sum)
